97971d615cd9ff9b2e8c168a015a7202ca8d36d1
jxv <javieramc2008@gmail.com>
2026-07-08 15:04:33 -0400

    Practica1Progra

--- src/main/java/logica/Main.java
+++ src/main/java/logica/Main.java
@@ -1,9 +1,15 @@
 package logica;
 
+import javax.swing.WindowConstants;
+import vista.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        login p1 = new login();
+        p1.setVisible(true);
+        p1.setLocationRelativeTo(null);
+        p1.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         
     }
 }
